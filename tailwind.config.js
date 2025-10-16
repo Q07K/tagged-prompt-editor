279265a4d89b3ef7b605f47f1de1dfaa22341f32
gyuhyeong <kgh0730@gmail.com>
2025-10-17 01:46:07 +0900

    Feat: Import custom colors and font family in Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+import customColors from './src/assets/theme/colors.js'
+import customFontFamily from './src/assets/theme/fontFamily.js'
+
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: customColors,
+      fontFamily: customFontFamily,
+    },
   },
   plugins: [],
 }
