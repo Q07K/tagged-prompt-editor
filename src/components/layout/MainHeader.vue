481bcd7dfee699ef1ee8864cb23a7cf91c760c64
gyuhyeong <kgh0730@gmail.com>
2025-10-18 01:28:56 +0900

    Refactor: Enhance MainHeader layout and styling for improved readability and theme integration

--- src/components/layout/MainHeader.vue
+++ src/components/layout/MainHeader.vue
@@ -4,6 +4,7 @@ import { computed } from 'vue'
 import { useTheme } from '@/composables/useTheme'
 import logoLight from '@/assets/icons/logo_light.svg'
 import logoDark from '@/assets/icons/logo_dark.svg'
+import colors from '@/assets/theme/colors'
 
 const { isDark } = useTheme()
 
@@ -12,18 +13,39 @@ const logoSrc = computed(() => {
 })
 </script>
 <template>
-  <header
-    class="bg-surface-light dark:bg-surface-dark border-b border-border-light dark:border-border-dark p-4 shadow-md"
-  >
-    <div class="flex flex-row items-center gap-2">
-      <img :src="logoSrc" alt="Tagged Prompt Editor logo" class="h-10 mb-2" />
-      <h1 class="text-xl font-bold text-primary">Tagged Prompt Editor</h1>
-    </div>
-    <div class="flex flex-row items-center justify-between gap-2">
-      <p class="text-sm text-text-main-light dark:text-text-main-dark mt-1">
-        왼쪽에 프롬프트를 입력/수정하면 오른쪽 블록이 실시간으로 업데이트됩니다.
-      </p>
+  <header class="main-header shadow-md">
+    <div class="flex flex-row items-start gap-2">
+      <img :src="logoSrc" alt="Tagged Prompt Editor logo" class="h-14 mb-2" />
+      <div class="flex flex-row items-center gap-auto w-full">
+        <div class="flex flex-col">
+          <h1 class="text-4xl font-bold text-primary">Tagged Prompt Editor</h1>
+          <p class="main-description text-sm mt-1">
+            LLM 프롬프트를 태그로 구성하고 실시간으로 편집 및 시각화하고
+            관리하세요.
+          </p>
+        </div>
+      </div>
       <ThemeToggle />
     </div>
   </header>
 </template>
+
+<style scoped>
+.main-header {
+  background-color: v-bind('colors["surface-light"]');
+  border-bottom: 1px solid v-bind('colors["border-light"]');
+  padding: 1rem;
+}
+.main-description {
+  color: v-bind('colors["text-main-light"]');
+  margin-top: 0.25rem;
+}
+
+.dark .main-header {
+  background-color: v-bind('colors["surface-dark"]');
+  border-bottom: v-bind('colors["border-dark"]');
+}
+.dark .main-description {
+  color: v-bind('colors["text-main-dark"]');
+}
+</style>
