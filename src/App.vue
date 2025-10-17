e4fcd449c010c7fcfba4beee0faeb3596aa19208
gyuhyeong <kgh0730@gmail.com>
2025-10-17 18:55:03 +0900

    Feat: Initialize theme on app mount and update background classes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import MainView from './views/MainView.vue'
+import { useTheme } from '@/composables/useTheme'
+
+const { initTheme } = useTheme()
+
+onMounted(() => {
+  initTheme()
+})
 </script>
 
 <template>
-  <div class="min-h-screen bg-gray-900">
+  <div class="min-h-screen bg-gray-900 dark:bg-gray-950">
     <MainView />
   </div>
 </template>
